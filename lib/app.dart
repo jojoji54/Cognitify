f4040a06079249efb5884c80a94ba546f61db6e8
Juan O <juanormaechea97@gmail.com>
2025-05-12 23:50:11 +0200

    creando el home

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:cognitify/screens/home_screen.dart';
+import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
+
+class CognitifyApp extends StatelessWidget {
+   CognitifyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NeumorphicApp(
+      title: 'Cognitify',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFE0E0E0),
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF2E3239),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: NeumorphicTheme.baseColor(context),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "Bienvenido a Cognitify",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            NeumorphicButton(
+              onPressed: () {},
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+              ),
+              child:  Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "Comenzar Pruebas Cognitivas",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700]),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            NeumorphicButton(
+              onPressed: () {},
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+              ),
+              child:  Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "Ver Resultados",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700]),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            NeumorphicButton(
+              onPressed: () {},
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "Configuración",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
